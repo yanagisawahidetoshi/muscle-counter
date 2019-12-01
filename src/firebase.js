859cba73d8109d521fb4d94d92c8b218eb3cf119
柳沢英俊 <master@ultrasevenstar.com>
2019-12-01 18:57:50 +0900

    FireaBaseJs追加

---
+++ src/firebase.js
@@ -0,0 +1,22 @@
+import firebase from 'firebase'
+import {
+  APIKEY,
+  AUTHDOMAIN,
+  DATABASEURL,
+  PROJECTID,
+  STORAGEBUCKET,
+  MESSAGINGSENDERID
+} from 'react-native-dotenv'
+
+const config = {
+  apiKey: APIKEY,
+  authDomain: AUTHDOMAIN,
+  databaseURL: DATABASEURL,
+  projectId: PROJECTID,
+  storageBucket: STORAGEBUCKET,
+  messagingSenderId: MESSAGINGSENDERID
+}
+
+firebase.initializeApp(config)
+
+export default firebase
